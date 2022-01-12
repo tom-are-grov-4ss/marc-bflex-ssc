4aa2ae08ec3b5d6b4d98c35f6650a468651c740e
tag <tag@4subsea.com>
2022-01-12 14:26:27 +0100

    correct names

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="marc_tools",
+    version="0.0.1",
+    license="Proprietary",
+    description="Tools for Marc",
+    url="http://www.4subsea.com/",
+    author="4Subsea",
+    author_email="tag@4subsea.com",
+    packages=[
+        "marc_tools",
+    ],
+    zip_safe=False,
+)
