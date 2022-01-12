5ad2b0974fffd9bfad80b8ae46af353696ef59d5
tag <tag@4subsea.com>
2022-01-12 14:59:16 +0100

    test

--- marc_tools/__init__.py
+++ marc_tools/__init__.py
@@ -1,8 +1,8 @@
 import pkg_resources
 import logging
 
-from . import general
-from . import scf
+# from . import general
+# from . import scf
 
 
 __version__ = pkg_resources.get_distribution('marc_tools').version
