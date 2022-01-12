3a48b87cc6af3e2214b8105b3af60a86a296ee22
tag <tag@4subsea.com>
2022-01-12 14:27:36 +0100

    update

---
+++ scf/__init__.py
@@ -0,0 +1,12 @@
+import pkg_resources
+import logging
+
+from . import general
+from . import scf
+
+
+__version__ = pkg_resources.get_distribution('marc_tools').version
+
+logging.basicConfig(filename=r'./Marc_tools.log', filemode='w',
+                    level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s')
